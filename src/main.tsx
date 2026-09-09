16f5f18a7ff1688e129d511a5eb47e22523a98ed
ruijpedro <rjpedro@gmail.com>
2026-09-09 14:09:06 +0100

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ErrorBoundary from './ErrorBoundary'
 import './styles.css'
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>)
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><ErrorBoundary><App/></ErrorBoundary></React.StrictMode>)
